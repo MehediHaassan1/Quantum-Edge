6daa61a866b303aee8fed4aa83b6010861940cc3
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2023-11-21 07:26:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <section>Home</section>
+            <section>Service</section>
+            <section>About</section>
+            <section>Product</section>
+            <section>Testimonial</section>
+            <section>Faq</section>
+        </div>
+    )
 }
 
-export default App;
+export default App
